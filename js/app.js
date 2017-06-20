4070b38914c194330013cf0c1d8f522bc2aa46cd
strengur <steingrimur@740.is>
2017-06-20 22:55:08 +0000

    Page now shows badges
    know more button in arsenal section has now js function to display all
    badges.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,49 @@
 var s2Content = document.getElementById('s2-content');
 var buttonArsenal = document.getElementById('button-arsenal');
 
-function getBatches () {
+function getBadges($arsenalButtonHTML) {
   var treehouseAPI = 'https://teamtreehouse.com/steingrimurstefansson.json?';
   var treehouseOptions = {
 
   }
 
   var $data;
-  function displayBatches(data) {
-    console.log(data);
-    $data = data.badges[49].icon_url;
+  function displayBadges(data) {
+    var $badgesCount = data.badges.length;
+    var $badgesObject = new Object();
+    var $listBadges = '<ul>';
+    for (i = 0; i < $badgesCount; i++) {
 
+      var $badgeNumber = Math.floor(Math.random() * $badgesCount);
+      $badgeUrl = data.badges[$badgeNumber].icon_url;
+      $badgeName = data.badges[$badgeNumber].name;
+
+      $badgesObject.image = $badgeUrl;
+      $badgesObject.name = $badgeName;
+      $listBadges += '<li><img src="' + $badgesObject.image + '" alt="' + $badgesObject.name + '">';
+      $listBadges += '<p>' + $badgesObject.name.toLowerCase() + '</p>';
+      $listBadges += '</li>';
+    }
+
+    $listBadges += '</ul>';
+    $listBadges += '</div>';
+    $('.badge-listing').html($listBadges);
+    $arsenalButtonHTML.html('<a href="">know less</a>');
   }
 
-  $.getJSON(treehouseAPI, treehouseOptions, displayBatches);
+  $.getJSON(treehouseAPI, treehouseOptions, displayBadges);
 }
+var $arsenalButtonStatus = 0;
+buttonArsenal.addEventListener('click', function(e) {
+  e.preventDefault();
+  var $arsenalButtonHTML = $(this);
 
-buttonArsenal.addEventListener('click', funditon() {
-  getBathes();
-})
+  if($arsenalButtonStatus <= 0) {
+    $arsenalButtonStatus = 1;
+    getBadges($arsenalButtonHTML);
+  } else {
+    $arsenalButtonStatus = 0;
+    $('.badge-listing').html('');
+    $arsenalButtonHTML.html('<a href="">know more</a>');
+  }
+});
