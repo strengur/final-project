3dcfd60072f230815b6b90afd3b0977046fba12c
strengur <steingrimur@740.is>
2017-06-24 13:42:23 +0000

    Hand in project 12

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 
 var $sectionOne = document.getElementById('s1');
+var $sectionTwo = document.getElementById('s2');
 var $sectionFour = document.getElementById('s4-content');
 var $buttonArsenal = document.getElementById('button-arsenal');
 var $buttonSense = document.getElementById('button-sense');
+var $buttonCloseSense = document.getElementById('close-sense');
 var $buttonTouch = document.getElementById('button-touch');
 var $nameTouch = document.getElementById('name');
 var $emailTouch = document.getElementById('email');
@@ -10,12 +12,12 @@ var $emailTouch = document.getElementById('email');
 // BEGIN: arsenal section interactivity when button 'know more' is clicked.
 function getBadges($arsenalButtonHTML) {
   var treehouseAPI = 'https://teamtreehouse.com/steingrimurstefansson.json?';
-  var treehouseOptions = {}
+  var treehouseOptions = {};
 
   var $data;
   function displayBadges(data) {
     var $badgesCount = data.badges.length;
-    var $badgesObject = new Object();
+    var $badgesObject = {};
     var $listBadges = '<ul>';
     for (i = 0; i < $badgesCount; i++) {
 
@@ -58,7 +60,17 @@ $buttonArsenal.addEventListener('click', function(e) {
 
 // BEGIN: sense section interactivity when button 'know more' is clicked.
 $buttonSense.addEventListener('click', function(e) {
-  e.preventDefault();
+	e.preventDefault();
+	$('.s3-content').fadeOut(400);
+	$('.projects-overview').delay(500).fadeIn(600);
+	window.scrollTo(0, $sectionOne.clientHeight + $sectionTwo.clientHeight);
+});
+
+$buttonCloseSense.addEventListener('click', function(e) {
+	e.preventDefault();
+	$('.projects-overview').fadeOut(400);
+	$('.s3-content').delay(500).fadeIn(600);
+	window.scrollTo(0, $sectionOne.clientHeight + $sectionTwo.clientHeight);
 });
 // END: sense section interactivity when button 'know more' is clicked.
 
@@ -71,7 +83,7 @@ function submitForm() {
   if($contactName !== '' && $emailAddress !== '') {
     $('#s4-content').fadeOut(700, function() {
       var $senseGreeting = '<div class="sense-message">';
-      $senseGreeting += '<div>'
+      $senseGreeting += '<div>';
       $senseGreeting += '<h3>hi ' + $contactName + '</h3>';
       $senseGreeting += '<p>thank you so much for contacting me!<br>i will do my very best to reply to your inquiry as sharply as i can, no later than next business day.</p>';
       $senseGreeting += '<p>have a great day!</p>';
@@ -88,7 +100,6 @@ function submitForm() {
         $('#s4-content').delay(200).fadeOut(300, function() {
           $('#s4-content').html($sectionFourContent).fadeIn();
           $buttonTouch = document.getElementById('button-touch');
-          console.log($buttonTouch);
         });
       });
     });
